e7f1ade3bd5e3d73dfa66e897c3355e4ace04512
Fabian M. Borschel <fmb@mailbox.org>
2014-10-31 07:38:32 +0100

    Require json and excon gems

--- lib/digitalocean-cli.rb
+++ lib/digitalocean-cli.rb
@@ -4,3 +4,5 @@ require 'digitalocean-cli/version.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+require 'excon'
+require 'json'
